50d536b029cb726b74284085cefd64118cfb80a5
Chris Wehner <chrisw7128@gmail.com>
2023-04-01 10:14:41 -0700

    added more css

--- script.js
+++ script.js
@@ -22,28 +22,77 @@ function deckBuilder() {
   ];
   const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
 
-  const cards = [];
+  const deck = [];
   for (let s = 0; s < suits.length; s++) {
     for (let v = 0; v < values.length; v++) {
       const value = values[v];
       const suit = suits[s];
-      cards.push({ value, suit });
+      deck.push({ value, suit });
     }
   }
-  return cards;
+  return deck;
 }
 
-const cards = deckBuilder();
+function clearCards() {
+  let attributes = [
+    "player1first",
+    "player1second",
+    "player2first",
+    "player2second",
+    "card1",
+    "card2",
+    "card3",
+    "card4",
+    "card5",
+  ];
+  for (let i = 0; i < attributes.length; i++) {
+    const card = document.getElementById(attributes[i]);
+    card.className = "";
+    card.innerHTML = "";
+  }
+}
+function setUp() {
+  clearCards();
 
-function randomCard(cards, div) {
-  const random = Math.floor(Math.random() * cards.length);
-  const cardValue = cards[random].value;
-  const cardSuit = cards[random].suit;
+  POT = 0;
+  PLAYER1.bet = 2;
+  PLAYER2.bet = 1;
+
+  const deck = deckBuilder();
+
+  let p1stack = document.getElementById("player1stack");
+  let p2stack = document.getElementById("player2stack");
+  p1stack.textContent = PLAYER1.stack;
+  p2stack.textContent = PLAYER2.stack;
+
+  let p1Bet = document.getElementById("p1Bet");
+  let p2Bet = document.getElementById("p2Bet");
+  p1Bet.textContent = PLAYER1.bet;
+  p2Bet.textContent = PLAYER2.bet;
+
+  let playerOneFirstCard = randomCard(deck, "player1first");
+  console.log(playerOneFirstCard);
+  let playerOneSecondCard = randomCard(deck, "player1second");
+
+  let playerTwoFirstCard = randomCard(deck, "player2first");
+  let playerTwoSecondCard = randomCard(deck, "player2second");
+
+  let card1 = randomCard(deck, "card1");
+  let card2 = randomCard(deck, "card2");
+  let card3 = randomCard(deck, "card3");
+  let card4 = randomCard(deck, "card4");
+  let card5 = randomCard(deck, "card5");
+}
+
+function randomCard(deck, divId) {
+  const random = Math.floor(Math.random() * deck.length);
+  const cardValue = deck[random].value;
+  const cardSuit = deck[random].suit;
   cardSuit === "Diamonds"
     ? (entity = "&diams;")
     : (entity = "&" + cardSuit.toLowerCase() + ";");
 
-  const card = document.getElementById(div);
+  const card = document.getElementById(divId);
   card.classList.add("card", cardSuit.toLowerCase());
   card.innerHTML =
     '<span class="card-value-suit top">' +
@@ -54,7 +103,7 @@ function randomCard(cards, div) {
     cardValue +
     entity +
     "</span>";
-  cards.splice(random, 1);
+  deck.splice(random, 1);
   return { value: cardValue, suit: cardSuit };
 }
 
@@ -68,25 +117,118 @@ function randomCard(cards, div) {
 //   console.log(PLAYER1.stack);
 // });
 
-let p1stack = document.getElementById("player1stack");
-let p2stack = document.getElementById("player2stack");
-p1stack.textContent = PLAYER1.stack;
-p2stack.textContent = PLAYER2.stack;
+async function p2Preflop() {
+  return new Promise(resolve => {
+    let raiseButton = document.getElementById("raiseButton");
+    let callButton = document.getElementById("callButton");
+    let foldButton = document.getElementById("foldButton");
+    raiseButton.addEventListener("click", raiseAction);
+    callButton.addEventListener("click", callAction);
+    foldButton.addEventListener("click", foldAction);
+    let p1stack = document.getElementById("player1stack");
+    let p2stack = document.getElementById("player2stack");
+    let p1Bet = document.getElementById("p1Bet");
+    let p2Bet = document.getElementById("p2Bet");
 
-let p1Bet = document.getElementById("p1Bet");
-let p2Bet = document.getElementById("p2Bet");
-p1Bet.textContent = PLAYER1.bet;
-p2Bet.textContent = PLAYER2.bet;
+    function raiseAction() {
+      // Do something when the raise button is clicked
+      PLAYER2.bet = 4;
+      p2Bet.textContent = PLAYER2.bet;
+      // End the user's turn
+      endTurn();
+    }
 
-let playerOneFirstCard = randomCard(cards, "player1first");
-console.log(playerOneFirstCard);
-let playerOneSecondCard = randomCard(cards, "player1second");
+    function callAction() {
+      // Do something when the call button is clicked
+      PLAYER2.bet = 2;
+      p2Bet.textContent = PLAYER2.bet;
+      // End the user's turn
+      endTurn();
+    }
 
-let playerTwoFirstCard = randomCard(cards, "player2first");
-let playerTwoSecondCard = randomCard(cards, "player2second");
+    function foldAction() {
+      // Do something when the fold button is clicked
+      PLAYER2.stack = PLAYER2.stack - PLAYER2.bet;
+      p2stack.textContent = PLAYER2.stack;
+      PLAYER1.stack = PLAYER1.stack + PLAYER2.bet;
+      p1stack.textContent = PLAYER1.stack;
+      PLAYER2.bet = 0;
+      // End the user's turn
+      endTurn();
+    }
 
-let card1 = randomCard(cards, "card1");
-let card2 = randomCard(cards, "card2");
-let card3 = randomCard(cards, "card3");
-let card4 = randomCard(cards, "card4");
-let card5 = randomCard(cards, "card5");
+    function endTurn() {
+      raiseButton.removeEventListener("click", raiseAction);
+      callButton.removeEventListener("click", callAction);
+      foldButton.removeEventListener("click", foldAction);
+      resolve();
+    }
+  });
+}
+
+function rngVsRaise() {
+  const outcomes = ["raise", "call", "fold"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return outcomes[randomIndex];
+}
+
+function rngVsCall() {
+  const outcomes = ["raise", "check"];
+  const randomIndex = Math.floor(Math.random() * 2);
+  return outcomes[randomIndex];
+}
+
+async function p1Preflop() {
+  if (PLAYER2.bet == 4) {
+    p1Action = rngVsRaise();
+    if (p1Action == "raise") {
+      PLAYER1.bet = PLAYER2.bet + 2;
+      p1Bet.textContent = PLAYER1.bet;
+    } else if (p1Action == "call") {
+      PLAYER1.bet = PLAYER2.bet;
+      p1Bet.textContent = PLAYER1.bet;
+    } else if (p1Action == "fold") {
+      PLAYER1.stack = PLAYER1.stack - PLAYER1.bet;
+      p1stack.textContent = PLAYER1.stack;
+      PLAYER2.stack = PLAYER2.stack + PLAYER1.bet;
+      p2stack.textContent = PLAYER2.stack;
+    }
+  }
+  if (PLAYER2.bet == 2) {
+    p1Action = rngVsCall();
+    if (p1Action == "raise") {
+      PLAYER1.bet = PLAYER2.bet + 2;
+      p1Bet.textContent = PLAYER1.bet;
+    } else if (p1Action == "check") {
+      PLAYER1.bet = PLAYER2.bet;
+      p1Bet.textContet = PLAYER1.bet;
+    }
+  }
+}
+
+function showFlop() {
+  let card1 = document.getElementById("card1");
+  let card2 = document.getElementById("card2");
+  let card3 = document.getElementById("card3");
+  card1.style.display = "inline-block";
+  card2.style.display = "inline-block";
+  card3.style.display = "inline-block";
+}
+
+async function checkState() {
+  if (PLAYER1.bet != PLAYER2.bet) {
+    runFunctions();
+  } else {
+    // deal flop
+    showFlop();
+  }
+}
+
+async function runFunctions() {
+  setUp();
+  await p2Preflop();
+  setTimeout(p1Preflop, 500);
+  setTimeout(checkState, 750);
+}
+
+runFunctions();
